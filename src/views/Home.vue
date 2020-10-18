e5209066b69d29ecccb8b4b7a6c38188bc6b0605
charliecap <charlesclinesdesk@gmail.com>
2020-10-18 10:07:37 -0400

    .

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Home</ion-title>
       </ion-toolbar>
     </ion-header>
     
@@ -14,7 +14,7 @@
       </ion-header>
     
       <div id="container">
-        <strong>Ready to create an app?</strong>
+        <strong>Stay focused, get shit done.</strong>
         <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
       </div>
     </ion-content>
